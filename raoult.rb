633c19995bb5558309e49bd08df70aed81b1290d
Thomas Deschamps <thomas.deschamps@gitguardian.com>
2021-09-15 14:56:45 +0200

    oh :poop: I commited my AWS key

--- raoult.rb
+++ raoult.rb
@@ -2,6 +2,6 @@ class Raoult
   def self.call
     p 'Hello'
     p 'mavicou'
-    TOKEN = 'a52186e35e9506f7ef05fbbcd5c2ca314f807fc84269b8ed0a9aa1fd0ec42b23'
+    TOKEN = 'a52186e35e9506f7ef05fbbcd431ca314f807fc84269b8ed0a9aa1fd0ec42b23'
   end
 end
