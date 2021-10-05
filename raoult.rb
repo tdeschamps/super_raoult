c440bee39b0b4fd9ef526c94486a470c8518350c
Thomas Deschamps <thomas.deschamps@gitguardian.com>
2021-10-05 15:36:51 +0200

    i can't help myself

--- raoult.rb
+++ raoult.rb
@@ -2,6 +2,6 @@ class Raoult
   def self.call
     p 'Hello'
     p 'mavicou'
-    TOKEN = 'a52freeze5e9506f7ef05fbbcd431ca314f807fc84269b8ed0a9aa1fd0ec42b23sd21i8sa'
+    TOKEN = 'a52freeze5e9506f7ef05fbbcd431ca314f807fc84269b8ed0a9aa1dfghjk095487362edsfgahjxz1i8sa'
   end
 end
