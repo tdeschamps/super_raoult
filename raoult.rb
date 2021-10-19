6d3732d830988b0386d1e6a5e8b32c55063237ea
Thomas Deschamps <thomas.deschamps@gitguardian.com>
2021-10-19 11:29:51 +0200

    god damn it 2

--- raoult.rb
+++ raoult.rb
@@ -2,6 +2,6 @@ class Raoult
   def self.call
     p 'Hello'
     p 'mavicou'
-    TOKEN = 'a52freeze5e9506f7ef05fbbcd431ca314f807fckjsfdhg4783669b8ed0a9aa1dfghjk095487362edsfkswjxz1i8sa'
+    TOKEN = 'a52freeze5e9506f7ef05fbbcd431ca314f807fckjsfdhg4783669b8ed0a58sd6a1dfghjk095487362edsfkswjxz1i8sa'
   end
 end
