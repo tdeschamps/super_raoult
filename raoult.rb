590b26afff0122325b060621708bf9b65dde99ab
Thomas Deschamps <thomas.deschamps@gitguardian.com>
2021-11-12 16:14:58 +0100

    cdsklcjsdijdsivjdf

--- raoult.rb
+++ raoult.rb
@@ -3,5 +3,6 @@ class Raoult
     p 'Hello'
     p 'mavicou'
     TOKEN = 'a01frdske5e9dskajdl6f7ef05fbbcd431cewlkfhdw23e32ejhasdcvjhwefgd473ury786:w8ckjsfdhg4783669b8ed0a58sd6a1dfghjk095487362edsfkswcjhdsjkchyd1i8sa'
+    PG_SQL = 'postgres://skdcjoei:skdjwoidjoiehcnansgwquieidp@toto.db.elephantsql.com/fdslkjfklejw'
   end
 end
